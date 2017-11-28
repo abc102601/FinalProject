2e85b7c86a8ea5e752e73b3b490015aad1ed5114
abc102601 <abc102601397@yahoo.com.tw>
2017-11-28 19:31:50 +0800

    新的怪物生成器

---
+++ MonsterGenerator/CommonGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace MonsterGenerator
+{
+	public class CommonGenerator : IMonsterGenerator
+	{
+		private float timeWait;
+		private GameObject monster;
+
+		public CommonGenerator (float time, GameObject obj)
+		{
+			timeWait = time;
+			monster = obj;
+		}
+
+		float getTimeWait() {
+			return timeWait;
+		}
+
+		GameObject getMonster() {
+			return monster;
+		}
+	}
+}
+
