75e1509f168ae90195df1c3cccda2f80f9fc03d9
abc102601 <abc102601397@yahoo.com.tw>
2017-11-28 19:38:37 +0800

    r

--- ChangeLevel.cs
+++ ChangeLevel.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ChangeLevel : MonoBehaviour {
+
+	public float[] wait_time;
+	public GameObject[] monster;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +14,22 @@ public class NewBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ("Test");
+		
+	}
+
+	IEnumerator monster_create()
+	{
+		for(int i=0;i<=wait_time.Length-1;i++)
+		{
+			yield return new WaitForSeconds (wait_time[i]);
+			Instantiate (monster [i], new Vector3 (0, 0, 0), transform.rotation);
+		}
+
+
+	}
+	public void onCreateMonsterClick()
+	{
+		StartCoroutine ("monster_create");
+
 	}
 }
